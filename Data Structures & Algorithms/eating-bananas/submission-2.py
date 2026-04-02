0588b2a787678b4c74590ea9d1cedb3dc1a8f978
epicgdog <91296067+epicgdog@users.noreply.github.com>
2026-04-01 21:09:43 -0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,29 @@
+import math
+
+class Solution:
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # max value of k has to be the max value of piles
+        # find the minimum; binary search between 1 and max value of k
+        # each pile will take at least 1 hour to finish; decimal will take more
+        k = max(piles)
+        l = 1
+        r = k
+        while l <= r:
+            m = l + (r-l) // 2
+            total_hours = 0
+            for pile in piles:
+                total_hours += math.ceil(pile/m)
+
+            if total_hours <= h:
+                k = m
+                r = m-1
+            if total_hours > h:
+                l = m+1
+                
+        return k
+
+        
+
+
+        
