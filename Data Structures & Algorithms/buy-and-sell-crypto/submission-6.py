584f5e16bf2e1915dc1e48691c11be0a6fea5f80
epicgdog <91296067+epicgdog@users.noreply.github.com>
2026-09-16 13:29:22 -0700

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # the idea here is to make a sliding window. 
+        # i just picture a trading vue thingy
+        # ride all the hills, and once negative, move the window over
+
+
+        starting = 0
+        max_profit = 0
+        for i in range(len(prices)):
+            # only add to the current if it is increasing
+            # else we will move the starting to that point and continue counting 
+            # as long as it is increasing
+            # we only care about increasing and never decreasting if we wnat to maximize profit
+            diff = prices[i] - prices[starting]
+            if diff < 0:
+                # decreasing, move startinga nd reset the profit counter
+                starting = i
+            else:
+                if diff > max_profit:
+                    max_profit = diff
+
+            
+        return max_profit
