a7c0201b3ddaf7d6b15a06831a008e8ddb202275
epicgdog <91296067+epicgdog@users.noreply.github.com>
2026-09-16 12:41:40 -0700

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # my idea is to do two pointers, with one pointer at one and the other at the other end
+        # don't i move the pointer wiht the bigger value?
+
+        left = 0
+        right = len(heights) - 1
+        max_area = 0
+
+        while left < right:
+            dist = right - left
+            min_val = min(heights[right], heights[left])
+            area = min_val * dist
+            max_area = max(area, max_area)
+
+            if min_val == heights[left]:
+                # move the smaller one, you want to look for nubmers to top the max
+                left += 1
+            else:
+                right -= 1
+        return max_area
+
+
+        
