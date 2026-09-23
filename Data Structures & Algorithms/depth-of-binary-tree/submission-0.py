a20b41fc8f99657e584b840a51677f96007ae204
epicgdog <91296067+epicgdog@users.noreply.github.com>
2026-09-23 10:30:14 -0700

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # we need to basically traverse the tree as far down as possible
+        # and then count how far it is
+        # i'm guessing our stop is when the root is null and we return like 0 or something
+        # or we could just calculate the max depth at each level and add thsoe together
+
+        if not root:
+            return 0
+
+        left_max = self.maxDepth(root.left) + 1
+        right_max = self.maxDepth(root.right) + 1
+
+        if left_max > right_max:
+            return left_max
+        
+        return right_max
+
+
+        
